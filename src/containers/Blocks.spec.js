88857ef51428c455c470d334c9d542d4b3db4fb2
Eduardo Yutaka Nakanishi <eduardo.nakanishi@gmail.com>
2021-07-20 17:51:57 -0300

    Fix tests

--- src/containers/Blocks.spec.js
+++ src/containers/Blocks.spec.js
@@ -5,7 +5,7 @@ import thunk from 'redux-thunk';
 import { Provider } from "react-redux";
 import ConnectedBlocks, { Blocks } from "./Blocks";
 
-describe("<Blcoks />", () => {
+describe("<Blocks />", () => {
   const actions = {
     fetchBlocks: jest.fn()
   };
@@ -19,6 +19,32 @@ describe("<Blcoks />", () => {
     ]
   };
 
+  const nodeA = {
+    url: 'https://thawing-springs-53971.herokuapp.com',
+    online: false,
+    name: 'Node 1',
+    loading: false,
+    blocks: {
+      loading: false,
+      list: []
+    }
+  }
+
+  const nodeB = {
+    url: 'https://secret-lowlands-62331.herokuapp.com',
+    online: false,
+    name: 'Node 2',
+    loading: false,
+    blocks: {
+      loading: false,
+      list: []
+    }
+  }
+
+  const nodes = {
+    list: [nodeA, nodeB]
+  };
+
   it("renders the blocks data", () => {
     const { getByText } = render(<Blocks actions={actions} blocks={blocks} />);
     const data = getByText(/The Human Torch/i);
@@ -28,10 +54,10 @@ describe("<Blcoks />", () => {
 
   it("should match snapshot", () => {
     const middlewares = [thunk];
-    const store = configureMockStore(middlewares)({blocks});
+    const store = configureMockStore(middlewares)({nodes});
     const { container } = render(
       <Provider store={store}>
-        <ConnectedBlocks />
+        <ConnectedBlocks node={nodeA} />
       </Provider>
     )
     expect(container.firstChild).toMatchSnapshot();
